6ccba0ada54f869b3affacf42013541000b017fb
Your Name <you@example.com>
2023-03-31 13:50:24 +0300

    week 4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Random rnd = new Random();
+
+int myRandom;
+
+
+for (int i = 0; i < 50; i++)
+{
+    myRandom = rnd.Next(1, 50);
+    Console.WriteLine($"{i + 1}. {myRandom}");
+}
+
+
+
+
+
+//for(int i = 0; i < 10; i++) // i++ ==> i + 1
+//{
+//    Console.WriteLine($"{i + 1} I will not zoom classes.");
+//}
